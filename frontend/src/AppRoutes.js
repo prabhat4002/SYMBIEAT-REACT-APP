d135a022343fdf43fbd03611f3aee8781cd1de58
prabhat <shankarprabhat121@gmail.com>
2024-11-06 04:57:20 +0530

    Created Food Page

--- frontend/src/AppRoutes.js
+++ frontend/src/AppRoutes.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Route, Routes} from 'react-router-dom';
 import HomePage from './pages/Home/HomePage';
+import FoodPage from './pages/Food/FoodPage';
 export default function AppRoutes() {
   return (
     <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/search/:searchTerm" element={<HomePage/>}/>
         <Route path="/tag/:tag" element={<HomePage/>}/>
+        <Route path="/food/:id" element={<FoodPage/>}/>
+
     </Routes>
   )
 }
