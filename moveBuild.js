0e015bb0360b00af0d0b5d716d88946e546cbc09
prabhat <shankarprabhat121@gmail.com>
2024-11-10 20:08:59 +0530

    render deploy minor changes

--- moveBuild.js
+++ moveBuild.js
@@ -1,12 +1,18 @@
-// moveBuild.js
 const fs = require('fs');
 const path = require('path');
 
 const source = path.join(__dirname, 'frontend', 'build');
 const destination = path.join(__dirname, 'backend', 'src', 'public');
 
+// Ensure the target directory exists
+if (!fs.existsSync(path.dirname(destination))) {
+  fs.mkdirSync(path.dirname(destination), { recursive: true });
+}
+
+// Remove the old destination directory if it exists
 fs.rmSync(destination, { recursive: true, force: true });
-fs.rename(source, destination, (err) => {
-    if (err) throw err;
-    console.log('Build folder moved successfully!');
-});
+
+// Move the build folder to the backend
+fs.renameSync(source, destination);
+
+console.log('Build folder moved successfully!');
